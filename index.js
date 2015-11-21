f96c696e5985f46ed8f5628871c7e7985845eb84
John Wheal <john@whealmedia.com>
2015-11-21 11:13:43 +0000

    Added camera module

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var tessel = require('tessel');
 var rfidlib = require('rfid-pn532');
+var camera = require('camera-vc0706').use(tessel.port['D']);
 
 var rfid = rfidlib.use(tessel.port['A']);
 
@@ -8,6 +9,18 @@ rfid.on('ready', function (version) {
 
   rfid.on('data', function(card) {
     console.log('UID:', card.uid.toString('hex'));
+    camera.takePicture(function(err, image) {
+      if (err) {
+        console.log('error taking image', err);
+      } else {
+        // Name the image
+        var name = 'picture-' + Math.floor(Date.now()*1000) + '.jpg';
+        // Save the image
+        console.log('Picture saving as', name, '...');
+        process.sendfile(name, image);
+        console.log('done.');
+      }
+    });
   });
 });
 
